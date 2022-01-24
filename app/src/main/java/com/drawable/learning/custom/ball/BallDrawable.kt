b337a272c732886d5c5fcbd4461cc1ebccc1be1f
JereChen <jerechen11@gmail.com>
2022-01-24 17:11:12 +0800

    draw ball line

--- app/src/main/java/com/drawable/learning/custom/ball/BallDrawable.kt
+++ app/src/main/java/com/drawable/learning/custom/ball/BallDrawable.kt
@@ -10,13 +10,63 @@ class BallDrawable : Drawable() {
         color = Color.parseColor("#D2691E")
     }
 
+    private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = 1f.px
+        color = Color.BLACK
+    }
+
     override fun draw(canvas: Canvas) {
+        val radius = bounds.width().toFloat() / 2
         canvas.drawCircle(
             bounds.width().toFloat() / 2,
             bounds.height().toFloat() / 2,
-            15f.px,
+            radius,
             paint
         )
+
+        //the vertical line of the ball
+        canvas.drawLine(
+            bounds.width().toFloat() / 2,
+            0f,
+            bounds.width().toFloat() / 2,
+            bounds.height().toFloat(),
+            linePaint
+        )
+        //the transverse line of the ball
+        canvas.drawLine(
+            0f,
+            bounds.height().toFloat() / 2,
+            bounds.width().toFloat(),
+            bounds.height().toFloat() / 2,
+            linePaint
+        )
+
+        val path = Path()
+        val sinValue = kotlin.math.sin(Math.toRadians(45.0)).toFloat()
+        //left curve
+        path.moveTo(radius - sinValue * radius,
+            radius - sinValue * radius
+        )
+        path.cubicTo(radius - sinValue * radius,
+            radius - sinValue * radius,
+            radius,
+            radius,
+            radius - sinValue * radius,
+            radius + sinValue * radius
+        )
+        //right curve
+        path.moveTo(radius + sinValue * radius,
+            radius - sinValue * radius
+        )
+        path.cubicTo(radius + sinValue * radius,
+            radius - sinValue * radius,
+            radius,
+            radius,
+            radius + sinValue * radius,
+            radius + sinValue * radius
+        )
+        canvas.drawPath(path, linePaint)
     }
 
     override fun setAlpha(alpha: Int) {
